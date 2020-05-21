3caf914a71d8cc0aeac9ae36aa8c877a4c62bb18
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2020-05-22 00:26:35 +0200

    Scaffolding ready

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import app from App;
+import initModel from './Model';
+import update from './Update';
+import view from './View';
+
+const node = document.getElementById('app');
+
+app(initModel, update, view, node);
