58f5d0381903b1104547363ee99b72c1be04686d
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2020-05-24 00:12:24 +0200

    CRUD completed

--- src/Update.js
+++ src/Update.js
@@ -1,3 +1,54 @@
-export default function update() {
+import * as R from 'ramda';
 
+const MSGS = {
+    'ADD_CITY': 'ADD_CITY',
+    'INPUT_CITY_NAME': 'INPUT_CITY_NAME',
+    'DELETE_CITY': 'DELETE_CITY',
 }
+
+export const addCityMsg = {
+    type: MSGS.ADD_CITY
+}
+
+export function inputCityMsg(name) {
+    return {
+        type: MSGS.INPUT_CITY_NAME,
+        name
+    }
+}
+
+export function deleteCityMsg(id) {
+    return {
+        type: MSGS.DELETE_CITY,
+        id
+    }
+}
+
+function deleteCity(model, id) {
+    console.log('delete city');
+    const cities = R.reject(c => c.id === id, model.cities);
+    return {...model, cities};
+}
+
+function update(model, action) {
+    switch (action.type) {
+        case MSGS.INPUT_CITY_NAME:
+            const {name} = action;
+            return {...model, name};
+        case MSGS.ADD_CITY:
+            const {cities, next_id} = model;
+            const newCity = {
+                id: next_id,
+                name: model.name,
+                temp: null,
+                low: null,
+                high: null
+            }
+            return {name: '', next_id: next_id+1, cities: [...cities, newCity]};
+        case MSGS.DELETE_CITY:
+            const {id} = action;
+            return deleteCity(model, id);
+    }
+}
+
+export default update;
--- src/View.js
+++ src/View.js
@@ -1,3 +1,80 @@
-export default function view() {
+import hh from 'hyperscript-helpers';
+import {h} from 'virtual-dom';
 
+import * as R from 'ramda';
+
+import {inputCityMsg, addCityMsg, deleteCityMsg} from './Update';
+
+const {pre, div, h1, h2, input, button, label, i} = hh(h);
+
+function displayCell(label, value) {
+    return div([
+        h2({className: 'f5 ma2 mt3'}, label),
+        div({className: 'f5 ma2'}, value)
+    ]);
 }
+
+function displayCity(dispatch, city) {
+    console.log('City:', city);
+    return div({
+        className: 'flex bg-light-blue mv3'
+    }, [
+        div({
+            className: 'w-50'
+        },
+            displayCell('Location', city.name),
+        ),
+        div({
+            className: 'w-50 flex justify-between'
+        }, [
+            displayCell('Temp', city.temp),
+            displayCell('Low', city.low),
+            displayCell('High', city.high)
+        ]),
+        i({
+            className: 'fa fa-trash pa2 pointer',
+            onclick: e => dispatch(deleteCityMsg(city.id))
+        })
+    ]);
+}
+
+function displayList(dispatch, model) {
+    console.log('Model inside displayList:', model);
+    return R.map(city => displayCity(dispatch, city), model.cities);
+}
+
+function displayForm(dispatch, model) {
+    return div([
+        label({
+            className: 'db mv2',
+            for: 'city'
+            },
+            'Location'
+        ),
+        input({
+            className: 'pv1 ph2',
+            id: 'city',
+            onchange: e => dispatch(inputCityMsg(e.target.value))
+        }),
+        button({
+            className: 'pv1 ph2 bg-green white ba b--dark-green pointer grow',
+            type: 'button',
+            onclick: e => dispatch(addCityMsg)
+        }, 'Add')
+    ]);
+}
+
+function view(dispatch, model) {
+    return div({
+            className: 'avenir w-50 mw6 center'
+        },
+        [
+            h1({className: 'bb'}, 'Weather APP'),
+            displayForm(dispatch, model),
+            displayList(dispatch, model),
+            pre(JSON.stringify(model, null, 2))
+        ]
+    );
+}
+
+export default view;
