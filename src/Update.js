e1b097a7ec98d9dd90d2d94a3bdee4aaddd75940
Gabriela Leoniec <gabriela.leoniec@bookassist.com>
2020-05-27 11:08:02 +0200

    Handling the connection to the server added

--- src/Update.js
+++ src/Update.js
@@ -4,6 +4,13 @@ const MSGS = {
     'ADD_CITY': 'ADD_CITY',
     'INPUT_CITY_NAME': 'INPUT_CITY_NAME',
     'DELETE_CITY': 'DELETE_CITY',
+    'HTTP_SUCCESS': 'HTTP_SUCCESS'
+}
+
+const API_KEY = '9879a5db906e2899bc512227960719bc';
+
+function weatherURL(city) {
+    return `api.openweathermap.org/data/2.5/weather?q=${encodeURI(city)}&appid=${API_KEY}`;
 }
 
 export const addCityMsg = {
@@ -24,10 +31,36 @@ export function deleteCityMsg(id) {
     }
 }
 
+export function httpSuccessMsg(id, response) {
+    return {
+        type: MSGS.HTTP_SUCCESS,
+        id,
+        response
+    }
+}
+
 function deleteCity(model, id) {
-    console.log('delete city');
     const cities = R.reject(c => c.id === id, model.cities);
     return {...model, cities};
+}
+
+function updateTemps(model, id, response) {
+    const {cities} = model;
+    const {temp, temp_low, temp_max} = R.pathOr({}, ['data', 'main'], response);
+
+    const updatedCities = R.map(city => {
+        if(city.id === id){
+            return {
+                ...city,
+                temp: Math.round(temp),
+                low: Math.round(temp_low),
+                high: Math.round(temp_max),
+            }
+        }
+        return city;
+    }, cities);
+
+    return {...model, cities: updatedCities}
 }
 
 function update(model, action) {
@@ -43,14 +76,18 @@ function update(model, action) {
             const newCity = {
                 id: next_id,
                 name: model.name,
-                temp: null,
-                low: null,
-                high: null
+                temp: '?',
+                low: '?',
+                high: '?'
             }
             return {name: '', next_id: next_id+1, cities: [...cities, newCity]};
         case MSGS.DELETE_CITY:
             const {id} = action;
             return deleteCity(model, id);
+        case MSGS.HTTP_SUCCESS:
+            const {idUpdate, response} = action;
+            console.log(response);
+            return updateTemps(model, idUpdate, response);
     }
 }
 
